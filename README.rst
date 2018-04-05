9162e593342a12a0da17e2485667871549e2ab42
Uroš Jevremović <jevremovic.uros91@gmail.com>
2018-04-05 18:22:24 +0200

    init commit

---
+++ README.rst
@@ -0,0 +1,29 @@
+Text-file-size-checker
+----
+
+Simple script for calculating the number of lines, words and
+characters in a text file
+
+Args:
+    filename: The name of the file to analyze.
+
+Raises:
+    IOError: If 'filename' does not exist or can't be read.
+
+Returns:
+    A tuple where the first element is the number of lines in
+    the file, the second element is the number of words and the
+    third element is the number of characters.
+
+You can install it with
+----
+
+.. code:: bash
+
+    $ pip install txt-file-size-checker
+
+And run it by typing
+----
+
+.. code:: bash
+    $ python3 txt-file-size-checker.py 'name_of_txt_file_to_read'
---
+++ txt-file-size-checker/txt-file-size-checker.py
@@ -0,0 +1,33 @@
+import sys
+
+
+def analyze_text(filename):
+    """Calculate the number of lines, words and characters in a file
+
+    Args:
+        filename: The name of the file to analyze.
+
+    Raises:
+        IOError: If 'filename' does not exist or can't be read.
+
+    Returns:
+        A tuple where the first element is the number of lines in
+        the file, the second element is the number of words and the
+        third element is the number of characters.
+    """
+    lines = 0
+    words = 0
+    characters = 0
+    with open(filename, "rt") as f:
+        for line in f:
+            lines += 1
+            words += len(line.split())
+            characters += len(line)
+    return lines, words, characters
+
+
+if __name__ == '__main__':
+    lines, words, characters = analyze_text(filename=sys.argv[1])
+    print(f"\nTotal number of lines in a text file is {lines}\n")
+    print(f"Total number of words in a text file is {words}\n")
+    print(f"Total number of characters in a text file is {characters}\n")
